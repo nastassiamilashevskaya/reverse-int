65968f272f4db0ed939acef4e983b58495331655
nastassiamilashevskaya <52507944+nastassiamilashevskaya@users.noreply.github.com>
2020-02-25 20:23:14 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = String(n).split('');
+        let sign = n.shift();
+        n = n.reverse().join('');
+    } else {
+        n = String(n).split('').reverse().join('');
+    }
+    return Number(n);
 }
